b86fbbfba0fb856cd965cd5b2af2231400a736a4
Roman <roman.matrosov.frontend@yandex.ru>
2022-09-04 22:24:51 +0300

    Returned normal settings

--- settings.py
+++ settings.py
@@ -6,19 +6,19 @@ class Settings():
         # Параметры экрана
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (120, 230, 130)
+        self.bg_color = (220, 230, 130)
 
         # Стартовое кол-во жизней корабля
         self.ship_limit = 3
 
         # Параметры снаряда
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (250, 60, 60)
         self.bullets_allowed = 3
 
         # Параметры пришельца
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
 
         # Темп ускорения игры
         self.speedup_scale = 1.1
